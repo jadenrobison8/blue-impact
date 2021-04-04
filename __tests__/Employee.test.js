81808d5ec3dd833963fb5037e7549039b93ce439
robisonja <jadenrobison8@gmail.com>
2021-04-04 12:46:48 -0600

    first test passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,10 +1,13 @@
 //tests for the employee class
+const Employee = require('../lib/Employee.js');
 
-//name
-
-//id
-
-//email
+test('creates new employee object', () => {
+    const employee = new Employee('Jaden', '35', 'jadenrobison8@gmail.com');
+    
+    expect(employee.name).toBe('Jaden');
+    expect(employee.id).toBe('35');
+    expect(employee.email).toBe('jadenrobison8@gmail.com')
+})
 
 //getName()
 
